4ba6cd89e5091c480307b02a571283fcec48a4bc
Hyro-LVA <daryllcarter4209@outlook.com>
2025-12-07 01:31:18 -0800

    Refactor color initialization in Particle constructor

--- Particle.cpp
+++ Particle.cpp
@@ -8,14 +8,14 @@ Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosit
                                                                                       m_numPoints(numPoints),
                                                                                        m_radiansPerSec((float)rand() / RAND_MAX * M_PI),
                                                                                        m_vx(100 + rand() % 401), m_vy(100 + rand() % 401),
-                                                                                       m_color1(Color::Cyan), m_color2(Color::Magenta), 
-rgbToHsv(m_color1, m_hue1, m_saturation1, m_value1); //<--rainbow, had to add to constructor
-rgbToHsv(m_color2, m_hue2, m_saturation2, m_value2); //<--rainbow, had to add to constructor
+                                                                                       m_color1(Color::Cyan), m_color2(Color::Magenta)
+
 {
     m_cartesianPlane.setCenter(0, 0);
     m_cartesianPlane.setSize(target.getSize().x, (-1.0) * target.getSize().y);
     m_centerCoordinate = target.mapPixelToCoords(mouseClickPosition, m_cartesianPlane);
-        
+    rgbToHsv(m_color1, m_hue1, m_saturation1, m_value1); //<--rainbow, had to add to constructor
+    rgbToHsv(m_color2, m_hue2, m_saturation2, m_value2); //<--rainbow, had to add to constructor
   float theta = (float)rand() / RAND_MAX * (M_PI / 2);
   float dTheta = 2 * M_PI / (numPoints - 1);
   for (int j = 0; j < numPoints; ++j) {
