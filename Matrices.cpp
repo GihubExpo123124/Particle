c00ed76a8507c5beae99422cf032ff8278dab3e4
Simar Anand <simaranand2024@gmail.com>
2025-12-06 18:34:09 -0800

    Made some changes to Particles.cpp along w/ adding Matrices.cpp from old proj

---
+++ Matrices.cpp
@@ -0,0 +1,90 @@
+#include "Matrices.h"
+
+namespace Matrices {
+    Matrix::Matrix(int _rows, int _cols) {
+        rows = _rows;
+        cols = _cols;
+
+        a.resize(_rows);
+        for (vector<double>& t : a) {
+            t.resize(cols);
+        }
+
+    }
+
+    Matrix operator+(const Matrix& a, const Matrix& b) {
+        if (a.getCols() != b.getCols() || a.getRows() != b.getRows()) {
+            throw runtime_error("Error: dimensions must agree");
+        }
+        Matrix randObj(a.getRows(), a.getCols());
+
+        for (int i = 0; i < a.getRows(); ++i) {
+            for (int j = 0; j < a.getCols(); ++j) {
+                randObj(i, j) = a(i, j) + b(i, j);
+            }
+        }
+
+        return randObj;
+
+
+    }
+
+    bool operator ==(const Matrix& a, const Matrix& b) {
+        if (a.getCols() != b.getCols() || a.getRows() != b.getRows())
+        {
+            return false;
+        }
+        for (int i = 0; i < a.getRows(); ++i) {
+            for (int j = 0; j < a.getCols(); ++j) {
+                if (abs(a(i, j) - b(i, j)) < 0.001) { return true; }
+
+
+
+            }
+        }
+
+        return false;
+
+    }
+
+    bool operator!=(const Matrix& a, const Matrix& b) {
+        return !(a == b);
+    }
+
+    ostream& operator<<(ostream& os, const Matrix& a) {
+
+        for (int i = 0; i < a.getRows(); ++i) {
+            for (int j = 0; j < a.getCols(); ++j) {
+                os << setw(10) << a(i, j) /*wow*/;
+            }
+
+            os << "\n";
+
+        }
+
+
+        return os;
+    }
+
+    Matrix operator*(const Matrix& a, const Matrix& b) {
+
+        if (a.getCols() != b.getRows()) {
+            throw runtime_error("Error: dimensions must agree");
+        }
+        double sum{ 0 };
+        Matrix c(a.getRows(), b.getCols());
+
+        for (int k = 0; k < b.getCols(); k++) {
+            for (int i = 0; i < a.getRows(); ++i) {
+                for (int j = 0; j < a.getCols() && j < b.getRows(); ++j) {
+                    sum += a(i, j) * b(j, k);
+                }
+
+                c(i, k) = sum;
+                sum = 0;
+            }
+        }
+
+        return c;
+    }
+}
--- Particle.cpp
+++ Particle.cpp
@@ -1,18 +1,20 @@
 #include "Particle.h"
+#include <SFML/System.hpp>
+using namespace std;
 
 
 Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosition) : m_A(2, numPoints), 
                                                                                        m_ttl(TTL), 
                                                                                       m_numPoints(numPoints),
-                                                                                       m_radiansPerSec((float)rand() / RAND_MAX * PI),
-                                                                                       m_cartesianPlane({0,0}, target.getSize().x, (-1.0)* target.getSize().y),
-                                                                                       m_centerCoordinate(m_cartesianPlane.getCenter());
+                                                                                       m_radiansPerSec((float)rand() / RAND_MAX * M_PI),
+                                                                                       m_cartesianPlane(Vector2f(0,0), Vector2f(target.getSize().x, (-1.0)* target.getSize().y)),
+                                                                                       m_centerCoordinate(m_cartesianPlane.getCenter()),
                                                                                        m_vx(100 + rand() % 401), m_vy(100 + rand() % 401),
                                                                                        m_color1(Color::Red), m_color2(Color::Blue)
 {
-  float theta = (float)rand() / RAND_MAX * (PI / 2);
-  float dTheta = 2 * PI / (numPoints - 1);
-  for (int j = 0; i < numPoints; ++j) {
+  float theta = (float)rand() / RAND_MAX * (M_PI / 2);
+  float dTheta = 2 * M_PI / (numPoints - 1);
+  for (int j = 0; j < numPoints; ++j) {
       float r = 20 + rand() % 61, dx, dy;
       dx = r * cos(theta);
       dy = r * sin(theta);
@@ -25,17 +27,54 @@ Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosit
 void Particle::draw(RenderTarget& target, RenderStates states) const {
     VertexArray lines(TriangleFan, m_numPoints + 1);
    
-    Vector2f center(target.mapCoordsToPixel(m_cartesianPlane));
+    Vector2f center(target.mapCoordsToPixel(m_centerCoordinate));
     lines[0].position = center;
-    lines[0].color = m_color;
+    lines[0].color = m_color1;
 
-    for (int j = 1; j < m_numPoints + 1; ++j) {
-        lines[j].position = m_A(0, j-1);
+    for (int j = 1; j < m_numPoints + 1; j++ ) {
+        lines[j].position = (Vector2f)target.mapCoordsToPixel(Vector2f(0, j-1));
+        lines[j].color = m_color2;
     }
 
-    lines[j].color = m_Color2;
     target.draw(lines);
 }
+
+void Particle::update(float DT) {
+    m_ttl -= DT;
+    rotate(DT * m_radiansPerSec);
+    scale(SCALE);
+    float dx{m_vx*DT}, dy;
+    m_vy -= G * DT;
+    dy = m_vy * DT;
+    translate(dx, dy);
+}
+
+void Particle::translate(double xShift, double yShift) {
+    TranslationMatrix T(xShift, yShift, 5);
+    m_A = T + m_A;
+    m_centerCoordinate.x += xShift;
+    m_centerCoordinate.y += yShift;
+}
+
+void Particle::rotate(double theta) {
+    Vector2f temp(m_centerCoordinate);
+    translate(-m_centerCoordinate.x, -m_centerCoordinate.y);
+    RotationMatrix R(theta);
+    m_A = R *m_A;
+    translate(temp.x, temp.y);
+}
+
+void Particle::scale(double c) {
+    Vector2f temp(m_centerCoordinate);
+    translate(-m_centerCoordinate.x, -m_centerCoordinate.y);
+    ScalingMatrix S(c);
+    m_A = S * m_A;
+    translate(temp.x, temp.y);
+
+
+}
+
+
 bool Particle::almostEqual(double a, double b, double eps)
 {
     return fabs(a - b) < eps;
@@ -45,7 +84,7 @@ void Particle::unitTests()
 {
     int score = 0;
 
-    cout << "Testing RotationMatrix constructor...";
+    std::cout << "Testing RotationMatrix constructor...";
     double theta = M_PI / 4.0;
     RotationMatrix r(M_PI / 4);
     if (r.getRows() == 2 && r.getCols() == 2 && almostEqual(r(0, 0), cos(theta))
@@ -53,15 +92,15 @@ void Particle::unitTests()
         && almostEqual(r(1, 0), sin(theta))
         && almostEqual(r(1, 1), cos(theta)))
     {
-        cout << "Passed.  +1" << endl;
+        std::cout << "Passed.  +1" << endl;
         score++;
     }
     else
     {
-        cout << "Failed." << endl;
+        std::cout << "Failed." << endl;
     }
 
-    cout << "Testing ScalingMatrix constructor...";
+    std::cout << "Testing ScalingMatrix constructor...";
     ScalingMatrix s(1.5);
     if (s.getRows() == 2 && s.getCols() == 2
         && almostEqual(s(0, 0), 1.5)
@@ -69,15 +108,15 @@ void Particle::unitTests()
         && almostEqual(s(1, 0), 0)
         && almostEqual(s(1, 1), 1.5))
     {
-        cout << "Passed.  +1" << endl;
+        std::cout << "Passed.  +1" << endl;
         score++;
     }
     else
     {
-        cout << "Failed." << endl;
+        std::cout << "Failed." << endl;
     }
 
-    cout << "Testing TranslationMatrix constructor...";
+    std::cout << "Testing TranslationMatrix constructor...";
     TranslationMatrix t(5, -5, 3);
     if (t.getRows() == 2 && t.getCols() == 3
         && almostEqual(t(0, 0), 5)
@@ -87,28 +126,28 @@ void Particle::unitTests()
         && almostEqual(t(0, 2), 5)
         && almostEqual(t(1, 2), -5))
     {
-        cout << "Passed.  +1" << endl;
+        std::cout << "Passed.  +1" << endl;
         score++;
     }
     else
     {
-        cout << "Failed." << endl;
+        std::cout << "Failed." << endl;
     }
 
 
-    cout << "Testing Particles..." << endl;
-    cout << "Testing Particle mapping to Cartesian origin..." << endl;
+    std::cout << "Testing Particles..." << endl;
+    std::cout << "Testing Particle mapping to Cartesian origin..." << endl;
     if (m_centerCoordinate.x != 0 || m_centerCoordinate.y != 0)
     {
-        cout << "Failed.  Expected (0,0).  Received: (" << m_centerCoordinate.x << "," << m_centerCoordinate.y << ")" << endl;
+        std::cout << "Failed.  Expected (0,0).  Received: (" << m_centerCoordinate.x << "," << m_centerCoordinate.y << ")" << endl;
     }
     else
     {
-        cout << "Passed.  +1" << endl;
+        std::cout << "Passed.  +1" << endl;
         score++;
     }
 
-    cout << "Applying one rotation of 90 degrees about the origin..." << endl;
+    std::cout << "Applying one rotation of 90 degrees about the origin..." << endl;
     Matrix initialCoords = m_A;
     rotate(M_PI / 2.0);
     bool rotationPassed = true;
@@ -116,22 +155,22 @@ void Particle::unitTests()
     {
         if (!almostEqual(m_A(0, j), -initialCoords(1, j)) || !almostEqual(m_A(1, j), initialCoords(0, j)))
         {
-            cout << "Failed mapping: ";
-            cout << "(" << initialCoords(0, j) << ", " << initialCoords(1, j) << ") ==> (" << m_A(0, j) << ", " << m_A(1, j) << ")" << endl;
+            std::cout << "Failed mapping: ";
+            std::cout << "(" << initialCoords(0, j) << ", " << initialCoords(1, j) << ") ==> (" << m_A(0, j) << ", " << m_A(1, j) << ")" << endl;
             rotationPassed = false;
         }
     }
     if (rotationPassed)
     {
-        cout << "Passed.  +1" << endl;
+        std::cout << "Passed.  +1" << endl;
         score++;
     }
     else
     {
-        cout << "Failed." << endl;
+        std::cout << "Failed." << endl;
     }
 
-    cout << "Applying a scale of 0.5..." << endl;
+    std::cout << "Applying a scale of 0.5..." << endl;
     initialCoords = m_A;
     scale(0.5);
     bool scalePassed = true;
@@ -139,22 +178,22 @@ void Particle::unitTests()
     {
         if (!almostEqual(m_A(0, j), 0.5 * initialCoords(0, j)) || !almostEqual(m_A(1, j), 0.5 * initialCoords(1, j)))
         {
-            cout << "Failed mapping: ";
-            cout << "(" << initialCoords(0, j) << ", " << initialCoords(1, j) << ") ==> (" << m_A(0, j) << ", " << m_A(1, j) << ")" << endl;
+            std::cout << "Failed mapping: ";
+            std::cout << "(" << initialCoords(0, j) << ", " << initialCoords(1, j) << ") ==> (" << m_A(0, j) << ", " << m_A(1, j) << ")" << endl;
             scalePassed = false;
         }
     }
     if (scalePassed)
     {
-        cout << "Passed.  +1" << endl;
+        std::cout << "Passed.  +1" << endl;
         score++;
     }
     else
     {
-        cout << "Failed." << endl;
+        std::cout << "Failed." << endl;
     }
 
-    cout << "Applying a translation of (10, 5)..." << endl;
+    std::cout << "Applying a translation of (10, 5)..." << endl;
     initialCoords = m_A;
     translate(10, 5);
     bool translatePassed = true;
@@ -162,20 +201,20 @@ void Particle::unitTests()
     {
         if (!almostEqual(m_A(0, j), 10 + initialCoords(0, j)) || !almostEqual(m_A(1, j), 5 + initialCoords(1, j)))
         {
-            cout << "Failed mapping: ";
-            cout << "(" << initialCoords(0, j) << ", " << initialCoords(1, j) << ") ==> (" << m_A(0, j) << ", " << m_A(1, j) << ")" << endl;
+            std::cout << "Failed mapping: ";
+            std::cout << "(" << initialCoords(0, j) << ", " << initialCoords(1, j) << ") ==> (" << m_A(0, j) << ", " << m_A(1, j) << ")" << endl;
             translatePassed = false;
         }
     }
     if (translatePassed)
     {
-        cout << "Passed.  +1" << endl;
+        std::cout << "Passed.  +1" << endl;
         score++;
     }
     else
     {
-        cout << "Failed." << endl;
+        std::cout << "Failed." << endl;
     }
 
-    cout << "Score: " << score << " / 7" << endl;
+    std::cout << "Score: " << score << " / 7" << endl;
 }
